ad75e841602d8cc94561646fcedb7dc75af365ea
Joanne Wu <mrjoanne@gmail.com>
2015-02-02 02:18:44 -0800

    night

--- static/js/responsive-nav.js
+++ static/js/responsive-nav.js
@@ -196,7 +196,7 @@
          */
         this.options = {
           animate: true,                    // Boolean: Use CSS3 transitions, true or false
-          transition: 284,                  // Integer: Speed of the transition, in milliseconds
+          transition: 434,                  // Integer: Speed of the transition, in milliseconds
           label: "MoodMap",                    // String: Label for the navigation toggle
           insert: "before",                 // String: Insert the toggle before or after the navigation
           customToggle: "",                 // Selector: Specify the ID of a custom toggle
