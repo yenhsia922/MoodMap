c1f41f93628cf389ed24f12d32f91b6755823c6a
Christian Memije <christianmemije@yahoo.com>
2015-02-19 19:10:04 -0800

    Getting there.

--- routes/friends.js
+++ routes/friends.js
@@ -1,7 +1,14 @@
-
+/*
 // Get all of our friend data
 var friends = require('../friends.json');
 
 exports.viewFriends = function(req, res){
 	res.json(friends);
+};*/
+
+// Get all of our friend data
+var friends = require('../friends.json');
+
+exports.viewFriends = function(req, res){
+	res.render('friends');
 };
