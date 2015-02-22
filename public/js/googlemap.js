2479b07b18b594d022de22b72b040603b47eed01
Joanne Wu <mrjoanne@gmail.com>
2015-02-22 02:45:52 -0800

    only one infowindow open at a time

--- public/js/googlemap.js
+++ public/js/googlemap.js
@@ -7,8 +7,8 @@ var lng;
 var marker;
 var i;
 var infowindow;
-var markerArray = new Array();
-var onlineUsers = new Array();
+var prevMarkerWindow = 0;
+
 $(document).ready(function()
 {
   //getLocation();
@@ -174,18 +174,18 @@ function success(position)
           title: result[i].timestamp
         });
         linkInfoWindow(markerx, map, infowindow, result[i].username, result[i].currentStatus, result[i].userId, result[i].latitude, result[i].longitude);
-        infowindow.setContent("<h6 style='min-width: 120px;'>" + "<h6 style='text-transform: uppercase;'><span style='color: #33cc66;'><b>" + result[i].username + "</b></span></h6><i>''" + result[i].currentStatus + "''</i><br><a href='#' data-toggle='modal' data-target='#viewProfileModal'><h6>See Profile</h6></a></h6>");
-        infowindow.open(map, markerx);
-        linkInfoWindow(markerx, map, infowindow, result[i].username, result[i].currentStatus, result[i].userId, result[i].latitude, result[i].longitude);
       }
     } //end callbackdata
   function linkInfoWindow(marker, map, infowindow, username, status, id, latitude, longitude)
   {
     google.maps.event.addListener(marker, 'click', function()
     {
+      if(!prevMarkerWindow == 0){
+        prevMarkerWindow.close();
+      }
       infowindow.setContent("<h6 style='min-width: 120px;'>" + "<h6 style='text-transform: uppercase;'><span style='color: #33cc66;'><b>" + username + "</b></span></h6><i>''" + status + "''</i><br><a href='#' data-toggle='modal' data-target='#viewProfileModal'><h6>See Profile</h6></a></h6>");
       infowindow.open(map, marker);
-      console.log(latitude + " / " + longitude);
+      prevMarkerWindow = infowindow;
     });
   }
   google.maps.event.addListener(marker, 'click', function()
