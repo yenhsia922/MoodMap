1c24ce61387b08f7d39a020bcf113f743481e04b
Joanne Wu <mrjoanne@gmail.com>
2015-02-21 17:13:47 -0800

    Added modal for profile, send message

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,47 @@
 /* Inpired by Jee Dribbble Shot ( http://dribbble.com/shots/770815-Login ) */ 
 /* coded by alireza attari ( @alireza_attari ) */
 
-
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').focus();
 })
 
 jQuery(function () {
     jQuery('[data-toggle=tooltip]').tooltip();
+});
+
+
+
+$(document).ready(function() {
+    $("#alerthide").hide();
+	console.log("Hided alert");
+})
+
+$("#addfriend").click(function(){
+	console.log("cliced");
+    $("#friendAdded").show();
+});
+
+/*Will show user's username if exists*/
+$('#sendMessageModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+  modal.find('.modal-body input').val(recipient)
+})
+
+$('#actionSuccess')({			/* TODO: Uncaught type error exception here */
+overlayClose: true,
+onShow: function() {
+    var timer;
+    $('#sendSucessModal').bind({
+        mouseenter: function() {
+            clearTimeout(timer);
+        },
+        mouseleave: function() {
+            timer = setTimeout($.modal.close, 3);
+        }
+    })
+    .trigger('mouseleave');
+}
 });
