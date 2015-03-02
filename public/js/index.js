2d20fa6201633e57a418b4beff030d7d6665c9d0
Joanne Wu <wujots@github.com>
2015-03-02 08:16:32 +0000

    woopra tracking

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,26 @@
-/* Inpired by Jee Dribbble Shot ( http://dribbble.com/shots/770815-Login ) */ 
-/* coded by alireza attari ( @alireza_attari ) */
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+  initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+  // add any functionality and listeners you want here
+
+  $(".version_a").click(function(){
+    //add your Woopra tracking code for version A's like button click event
+    woopra.track("a_version_ham_click");
+  })
+
+  $(".version_b").click(function(){
+    //add your Woopra tracking code for version A's like button click event
+    woopra.track("b_version_ham_click");
+  })
+}
 
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').focus();
